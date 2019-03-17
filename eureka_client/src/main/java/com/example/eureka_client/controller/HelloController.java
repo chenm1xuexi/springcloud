b6fb38da19d17cc0d06185c77ecc0b0871d8f357
chenm1xuexi <837698719@qq.com>
2019-03-17 14:10:08 +0800

    update .gitignore

--- eureka_client/src/main/java/com/example/eureka_client/controller/HelloController.java
+++ eureka_client/src/main/java/com/example/eureka_client/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.eureka_client.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/hello")
 public class HelloController {
 
+    @Value("${test.gibson}")
+    private String userName;
+
     @RequestMapping(value = "/hi")
     public String hello() {
-        return "Hello, World!!!";
+        return "Hello, World!!!" + " my name is " + userName;
     }
 }
