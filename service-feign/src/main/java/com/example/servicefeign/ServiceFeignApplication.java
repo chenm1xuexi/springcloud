888a842005c20c71f5b0a55f03bdfd96c24d2a1f
shixiongfei <shixiongfei@3tlife.cn>
2019-03-29 15:14:04 +0800

    添加了链路追踪

--- service-feign/src/main/java/com/example/servicefeign/ServiceFeignApplication.java
+++ service-feign/src/main/java/com/example/servicefeign/ServiceFeignApplication.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableDiscoveryClient
 @EnableFeignClients
 public class ServiceFeignApplication {
 
--- service-feign/src/main/java/com/example/servicefeign/controller/HelloController.java
+++ service-feign/src/main/java/com/example/servicefeign/controller/HelloController.java
@@ -15,4 +15,9 @@ public class HelloController {
     public String hello() {
         return demoService.sayHello();
     }
+
+    @GetMapping(value = "/hi")
+    public String hello1() {
+        return "i am bibi";
+    }
 }
